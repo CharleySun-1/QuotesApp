101ed7816fded2ab7a72f2952ac3bae48183a8e0
CharleySun <Ysun@lcs.on.ca>
2021-02-19 11:52:07 -0500

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Charley Sun on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
